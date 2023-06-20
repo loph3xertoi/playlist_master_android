197148fdd95c08c4472281f51f2fca06dedc2798
Daw Loph <loph3xertoi@gmail.com>
2023-06-20 15:20:36 +0800

    Update the mock data.

--- lib/entities/song.dart
+++ lib/entities/song.dart
@@ -4,10 +4,12 @@ class Song {
   String name;
   List<Singer> singers;
   String coverUri;
-  
+  String link;
+
   Song({
     required this.name,
     required this.singers,
     required this.coverUri,
+    required this.link,
   });
 }
