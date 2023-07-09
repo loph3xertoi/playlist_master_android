aa5e09e992d9b279f97dc1fe83d0f5d91f0cdfc5
Daw Loph <loph3xertoi@gmail.com>
2023-07-09 09:47:37 +0800

    Fix bug: Move the logic of navigation to home page to didChangeDependencies.

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -2,12 +2,22 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
     Timer(Duration(seconds: 1), () {
       Navigator.pushReplacementNamed(context, '/home');
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SizedBox(
         width: double.infinity,
